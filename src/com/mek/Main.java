e5c861140b662ee41cf4e7930340011d119b6666
Musa Kocabas <mek26mek@gmail.com>
2021-03-14 13:25:55 +0300

    casting

--- src/com/mek/Main.java
+++ src/com/mek/Main.java
@@ -3,6 +3,14 @@ package com.mek;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    byte byteValue = 120;
+	    short shortValue = 15;
+		int intValue = 250;
+
+		long longValue = 50000L + (10 * (byteValue+shortValue+intValue));
+		//System.out.println("total = "+ longValue);
+		short shortTotal = (short)(byteValue+shortValue+intValue);
+		byte byteTotal = (byte)(byteValue+shortValue+intValue);
+		System.out.println("long value = "+ longValue + " short total = "+ shortTotal + " byte total = " + byteTotal);
     }
 }
